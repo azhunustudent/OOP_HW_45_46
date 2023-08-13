d4686fb6ebd52efc9342e8142571dcfdaf51de4b
begimay <student.zhunusova@gmail.com>
2023-08-14 04:18:32 +0600

    Добавьте файлы проекта.

---
+++ OOP_HW_45_46/ClassEquation.cpp
@@ -0,0 +1,181 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <math.h>
+using namespace std;
+#define RAND(Min,Max)  (rand()%((Max)-(Min)+1)+(Min))
+struct LC { LC() { system("chcp 1251 > nul"); srand(time(0)); }~LC() { cin.get(); cin.get(); } } _;
+
+
+// Задание 1.
+// Создать виртуальный базовый класс с виртуальной функцией — корни уравнения.
+// Создать производные классы:
+// 1) класс линейных уравнений;
+// 2) класс квадратных уравнений.
+// Написать функцию, которая по выбору пользователя создает экземпляр класса Liner или SquareEqu 
+// (функция "фабрики класса" по аналогии с CreateShape()) и 
+// возвращает через указатель на базовый класс Equation.
+
+// Базовый класс с виртуальной функцией — корни уравнения
+class Equation 
+{
+private:
+    string Name;
+public:
+    Equation(string name) : Name(name) {}
+    Equation() = delete;
+
+    string GetName() const { return Name; }
+
+    virtual void Calc() = 0;
+    virtual void Input() = 0;
+
+    virtual void Print()
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Name: " << GetName() << endl;
+    }
+
+    virtual ~Equation() {}
+};
+
+
+// Класс линейных уравнений
+class Liner : virtual public Equation
+{
+private:
+    float a, b;
+protected:
+    float x;
+public:
+    Liner() : Equation("Liner"), a(), b() {}
+    Liner(float val1, float val2) : Equation("Liner"), a(val1), b(val2) {}
+
+    virtual void Calc() override
+    {
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                cout << "Error: Infinite number of roots!" << endl;
+            }
+            else
+            {
+                cout << "No roots" << endl;
+            }
+        }
+        else
+        {
+            x = -b / a;
+        }
+    }
+
+    virtual void Input() override
+    {
+        cout << "Enter a and b coefficients: ";
+        (cin >> a >> b).ignore();
+    }
+
+    virtual void Print() override
+    {
+        Equation::Print();
+        // Пример: 3x - 2 = 0
+        cout << "x = " << x << endl;
+    }
+
+    virtual ~Liner() {}
+};
+
+
+// Класс квадратных уравнений
+class SquareEqu : virtual public Equation
+{
+private:
+    float a, b, c;
+protected:
+    float D, x1, x2, x3;
+public:
+    SquareEqu() : Equation("SquareEqu"), a(), b(), c() {}
+    SquareEqu(float val1, float val2, float val3) : Equation("SquareEqu"), a(val1), b(val2), c(val3) {}
+
+    virtual void Calc() override
+    {
+        D = pow(b, 2) - 4 * a * c;
+        if (D > 0)
+        {
+            x1 = ((-b) + sqrt(D)) / (2 * a);
+            x2 = ((-b) - sqrt(D)) / (2 * a);
+        }
+        else if (D == 0)
+        {
+            x3 = (- b) / (2 * a);
+        }
+    }
+    virtual void Input() override
+    {
+        cout << "Enter a, b and c coefficients: ";
+        (cin >> a >> b >> c).ignore();
+    }
+
+    virtual void Print() override
+    {
+        Equation::Print();
+        if (D > 0)
+        {
+            // Пример: 14x^2 - 5x - 1 = 0
+            cout << "x1 = " << x1 << "\nx2 = " << x2 << endl;
+        }
+        else if (D == 0)
+        {
+            // Пример: 9x^2 - 30x + 25 = 0
+            cout << "x = " << x3 << endl;
+        }
+        else
+        {
+            // Пример: 2x^2 + x + 67 = 0
+            cout << "No roots" << endl;
+        }
+    }
+
+    virtual ~SquareEqu() {}
+};
+
+
+// функция "Фабрики класса Equation" 
+Equation* CreateEquation()
+{
+    int choice;
+    cout << "1. Liner." << endl;
+    cout << "2. SquareEqu." << endl;
+    cout << "0. Exit." << endl;
+    cout << "Enter choice: ";
+    (cin >> choice).ignore();
+
+    switch (choice)
+    {
+        case 1: return new Liner;
+        case 2: return new SquareEqu;
+        case 0: return nullptr;
+    }
+
+    return nullptr;
+}
+
+int main()
+{
+    for(;;)
+    {
+        Equation* equation = CreateEquation();
+
+        if (equation == nullptr) { break; }
+        cout << "--------------------------------------------------\n";
+        equation->Input();
+        cout << "--------------------------------------------------\n";
+        equation->Calc();
+        equation->Print();
+        cout << "--------------------------------------------------\n";
+        delete equation;
+    }
+    cout << "Good bye...\n";
+}
---
+++ OOP_HW_45_46/ClassPet.cpp
@@ -0,0 +1,277 @@
+﻿#include <iostream>
+#include <string>
+#include <time.h>
+using namespace std;
+#define RAND(Min,Max)  (rand()%((Max)-(Min)+1)+(Min))
+struct LC { LC() { system("chcp 1251 > nul"); srand(time(0)); }~LC() { cin.get(); cin.get(); } } _;
+
+
+// Задание 2
+// Создать абстрактный базовый класс "Домашнее животное" с виртуальными функциями - методами
+// Sound() - издает звук животного(пишем текстом в консоль);
+// Show() - отображает имя животного;
+// Type() - отображает название его подвида.
+// Создайте производные классы "Собака", "Кошка", "Попугай", "Хомяк".
+// С помощью конструктора установить имя каждого животного и его характеристики.
+// В каждом дочернем классе переопределите виртуальные функции базового класса.
+// Написать функцию, которая по выбору пользователя создает экземпляр класса "Собака", "Кошка", "Попугай", "Хомяк" (функция "фабрики класса" по аналогии с CreateShape()) и возвращает через указатель на базовый класс "Домашнее животное".
+
+// А бстрактный базовый класс "Домашнее животное"
+class Pet
+{
+private:
+    string Name;
+protected:
+    string Breed;
+public:
+    Pet() = delete;
+    Pet(string name) : Name(name) {}
+
+    string GetName()  const { return Name; }
+    string GetBreed() const { return Breed; }
+
+    void SetName(string val)  { Name = val; }
+    void SetBreed(string val) { Breed = val; }
+
+    virtual void Input()
+    {
+        cout << "Enter info for " << typeid(*this).name() << endl;
+
+        string tmp;
+        cout << "Enter breed: ";
+        getline(cin, tmp);
+        SetBreed(tmp);
+    }
+
+    // Sound() - издает звук животного(пишем текстом в консоль);
+    virtual void Sound() = 0;
+
+    // Show() - отображает имя животного;
+    virtual void Show()
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Name: " << GetName() << endl;
+    }
+
+    // Type() - отображает название его подвида.
+    virtual void Type() = 0;
+
+    virtual ~Pet() {}
+};
+
+
+// Класс "Собака"
+class Dog : virtual public Pet
+{
+protected:
+    string Toy;			// любимая игрушка собаки
+public:
+    Dog() : Pet("Dog") {}
+    Dog(string toy, string breed) : Pet("Dog"), Toy(toy) {}
+
+    string GetToy()   const { return Toy; }
+    void SetToy(string val) { Toy = val; }
+
+    virtual void Input() override
+    {
+        Pet::Input();
+        string tmp;
+        cout << "Enter favorite toy: "; 
+        getline(cin, tmp); 
+        SetToy(tmp);
+    }
+
+    virtual void Sound() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Sound: woof-woof" << endl;
+    }
+
+    virtual void Show() override
+    {
+        Pet::Show();
+        cout << "Toy: " << GetToy() << endl;
+    }
+
+    virtual void Type() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Breed: " << GetBreed() << endl;
+    }
+    
+    virtual ~Dog() override {}
+};
+
+
+// Класс "Кошка"
+class Cat : virtual public Pet
+{
+private:
+    int LifeCount;
+public:
+    Cat() : Pet("Cat"), LifeCount() {}
+    Cat(int count) : Pet("Cat"), LifeCount(count) {}
+
+    int GetLifeCount()   const { return LifeCount; }
+    void SetLifeCount(int val) { LifeCount = val; }
+
+    virtual void Input() override
+    {
+        Pet::Input();
+        int tmp;
+        cout << "Enter life count: ";
+        (cin >> tmp).ignore();
+        SetLifeCount(tmp);
+    }
+
+    virtual void Sound() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Sound: meow-meow" << endl;
+    }
+
+    virtual void Show() override
+    {
+        Pet::Show();
+        cout << "Life Count: " << GetLifeCount() << endl;
+    }
+
+    virtual void Type() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Breed: " << GetBreed() << endl;
+    }
+
+    virtual ~Cat() override {}
+};
+
+
+// Класс "Попугай"
+class Parrot : virtual public Pet
+{
+private:
+    string Word;		// любимое или повторяемое слово попугая(?)
+public:
+    Parrot() : Pet("Parrot") {}
+    Parrot(string word) : Pet("Parrot"), Word(word) {}
+
+    string GetWord() const { return Word; }
+    void SetWord(string val) { Word = val; }
+
+    virtual void Input() override
+    {
+        Pet::Input();
+        string tmp;
+        cout << "Enter favorite word: ";
+        getline(cin, tmp);
+        SetWord(tmp);
+    }
+
+    virtual void Sound() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Sound: squawk-squawk" << endl;
+    }
+
+    virtual void Show() override
+    {
+        Pet::Show();
+        cout << "Word: " << GetWord() << endl;
+    }
+
+    virtual void Type() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Breed: " << GetBreed() << endl;
+    }
+
+    virtual ~Parrot() override {}
+};
+
+
+// Класс "Хомяк"
+class Hamster : virtual public Pet
+{
+private:
+    string Food;		// любимая еда
+public:
+    Hamster() : Pet("Hamster") {}
+    Hamster(string food) : Pet("Hamster"), Food(food) {}
+
+    string GetFood() const { return Food; }
+    void SetFood(string val) { Food = val; }
+
+    virtual void Input() override
+    {
+        Pet::Input();
+        string tmp;
+        cout << "Enter favorite food: ";
+        getline(cin, tmp);
+        SetFood(tmp);
+
+    }
+    virtual void Sound() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Sound: squeak-squeak" << endl;
+    }
+
+    virtual void Show() override
+    {
+        Pet::Show();
+        cout << "Food: " << GetFood() << endl;
+    }
+
+    virtual void Type() override
+    {
+        cout << typeid(*this).name() << endl;
+        cout << "Breed: " << GetBreed() << endl;
+    }
+
+    virtual ~Hamster() override {}
+};
+
+// функция "Фабрики класса Pet" 
+Pet* CreatePet()
+{
+    int choice;
+    cout << "1. Dog." << endl;
+    cout << "2. Cat." << endl;
+    cout << "3. Parrot." << endl;
+    cout << "4. Hamster." << endl;
+    cout << "0. Exit." << endl;
+    cout << "Enter choice: ";
+    (cin >> choice).ignore();
+
+    switch (choice)
+    {
+    case 1: return new Dog;
+    case 2: return new Cat;
+    case 3: return new Parrot;
+    case 4: return new Hamster;
+    case 0: return nullptr;
+    }
+
+    return nullptr;
+}
+
+int main()
+{
+    for (;;)
+    {
+        Pet* pet = CreatePet();
+
+        if (pet == nullptr) { break; }
+        cout << "--------------------------------------------------\n";
+        pet->Input();
+        cout << "--------------------------------------------------\n";
+        pet->Sound();
+        cout << "--------------------------------------------------\n";
+        pet->Show();
+        cout << "--------------------------------------------------\n";
+        pet->Type();
+        cout << "--------------------------------------------------\n";
+        delete pet;
+    }
+    cout << "Good bye...\n";
+}
